088a69d0e5b575e3e7fd0b4e24de285be54a0664
yetian <yetian.mao@japancv.co.jp>
2022-04-30 03:23:06 +0800

    feat: basic app layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
-import { Button } from 'antd';
+import { Tabs } from 'antd';
+import Upload from './components/Upload';
+import Search from './components/Search';
 import './App.css';
 
+const { TabPane } = Tabs;
+
 const App = () => (
-  <div className="App">
-    <Button type="primary">Button</Button>
+  <div className="container mx-auto p-4">
+    <Tabs defaultActiveKey="1">
+      <TabPane tab="Upload" key="1">
+        <Upload />
+      </TabPane>
+      <TabPane tab="Search" key="2">
+        <Search />
+      </TabPane>
+    </Tabs>
   </div>
 );
 
---
+++ src/components/Search.tsx
@@ -0,0 +1,12 @@
+import { Image } from 'antd';
+
+const Search = () => {
+  return (
+    <Image
+      width={200}
+      src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
+    />
+  );
+};
+
+export default Search;
