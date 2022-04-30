943340ee6802de7ec6b0e0b0d666a552c9e2836f
yetian <yetian.mao@japancv.co.jp>
2022-04-30 17:22:09 +0800

    feat: layout search page

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,5 +1,9 @@
-import { Upload, message, UploadProps } from 'antd';
-import { LoadingOutlined, PlusOutlined } from '@ant-design/icons';
+import { Upload, message, UploadProps, Button, Image } from 'antd';
+import {
+  LoadingOutlined,
+  PlusOutlined,
+  SearchOutlined,
+} from '@ant-design/icons';
 import { useState } from 'react';
 
 const getBase64 = (
@@ -11,9 +15,30 @@ const getBase64 = (
   reader.readAsDataURL(img as unknown as Blob);
 };
 
+type SimilarImages = {
+  url: string;
+  similarityScore: number;
+};
+
+const images: SimilarImages[] = [
+  {
+    url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+    similarityScore: 0.99,
+  },
+  {
+    url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+    similarityScore: 0.99,
+  },
+  {
+    url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+    similarityScore: 0.99,
+  },
+];
+
 const Search = () => {
   const [loading, setLoading] = useState(false);
   const [imageUrlBase64, setImageUrlBase64] = useState<string | null>(null);
+  const [similarImages, setSimilarImages] = useState<SimilarImages[]>(images);
   const uploadButton = (
     <div>
       {loading ? <LoadingOutlined /> : <PlusOutlined />}
@@ -22,7 +47,6 @@ const Search = () => {
   );
   const props: UploadProps = {
     name: 'search',
-    className: 'avatar-uploader',
     action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
     beforeUpload: (file) => {
       const isImage = file.type.includes('image/');
@@ -52,13 +76,41 @@ const Search = () => {
     listType: 'picture-card',
   };
   return (
-    <Upload {...props}>
-      {imageUrlBase64 ? (
-        <img src={imageUrlBase64} alt="avatar" style={{ width: '100%' }} />
-      ) : (
-        uploadButton
-      )}
-    </Upload>
+    <>
+      <div className="flex justify-center">
+        <div className="flex flex-col justify-self-center items-center w-48">
+          <Upload {...props}>
+            {imageUrlBase64 ? (
+              <img
+                src={imageUrlBase64}
+                alt="avatar"
+                style={{ width: '100%' }}
+              />
+            ) : (
+              uploadButton
+            )}
+          </Upload>
+          <Button
+            className="w-48 my-3"
+            type="primary"
+            icon={<SearchOutlined />}
+          >
+            Search
+          </Button>
+        </div>
+      </div>
+      <div className="flex justify-center my-3">
+        {similarImages.map((similarImage, index) => (
+          <div key={index} className="mx-2 flex flex-col items-center">
+            <Image width={200} src={similarImage.url} preview={false} />
+            <div className="underline decoration-sky-500/30 text-xl my-2">
+              Similarity:
+            </div>
+            <div className="text-base">{similarImage.similarityScore}</div>
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 
