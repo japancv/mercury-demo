31b53a554d84d1433e59d6106b031f39d73f89d0
myt588 <yetian.mao@gmail.com>
2022-05-13 00:56:05 +0800

    feat: add firebase storage setup

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,19 @@ import './App.css';
 
 const { TabPane } = Tabs;
 
-const App = () => (
-  <div className="container mx-auto p-4">
-    <Tabs defaultActiveKey="1">
-      <TabPane tab="Upload" key="1">
-        <Upload />
-      </TabPane>
-      <TabPane tab="Search" key="2">
-        <Search />
-      </TabPane>
-    </Tabs>
-  </div>
-);
+const App = () => {
+  return (
+    <div className="container mx-auto p-4">
+      <Tabs defaultActiveKey="1">
+        <TabPane tab="Upload" key="1">
+          <Upload />
+        </TabPane>
+        <TabPane tab="Search" key="2">
+          <Search />
+        </TabPane>
+      </Tabs>
+    </div>
+  );
+};
 
 export default App;
