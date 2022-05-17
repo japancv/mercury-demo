76e02f7841965fb9392d31162ec06107fd9ed31b
myt588 <yetian.mao@gmail.com>
2022-05-17 23:22:22 +0800

    feat: delete all files every day

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,22 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info('Hello logs!', { structuredData: true });
-  response.send('Hello from Firebase!');
+admin.initializeApp({
+  storageBucket: 'gcp-tech-dev.appspot.com',
 });
+
+const storage = admin.storage();
+
+const today = new Date().toLocaleDateString().replace(/\//g, '-');
+
+process.env.TZ = 'Asia/Tokyo'; // set timezone to tokyo
+
+exports.cleanupDataDaily = functions.pubsub
+  .schedule('59 8 * * *') // runs every night at 23:59
+  .region('asia-northeast1')
+  .onRun(() => {
+    const bucket = storage.bucket();
+    bucket.deleteFiles({
+      prefix: `${today}/`,
+    });
+  });
