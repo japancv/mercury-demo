68bbd21016f2f0dab031b2eecc53f5d7869a1fc8
myt588 <yetian.mao@gmail.com>
2022-05-18 15:29:44 +0800

    chore: update proxy server

--- functions/cors.js
+++ functions/cors.js
@@ -1,54 +1,46 @@
 'use strict';
 
 const functions = require('firebase-functions');
+const cors = require('cors');
 
-const fetch = require('node-fetch');
+const request = require('request');
 
 exports.cors = functions
   .region('asia-northeast1')
   .https.onRequest((req, res) => {
-    // Set CORS headers: allow all origins, methods, and headers: you may want to lock this down in a production environment
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, PUT, PATCH, POST, DELETE');
-    res.header(
-      'Access-Control-Allow-Headers',
-      req.header('access-control-request-headers')
-    );
+    cors()(req, res, () => {
+      if (req.method === 'OPTIONS') {
+        // CORS Preflight
+        res.send();
+      } else {
+        console.log('Query:', req.query);
+        console.log('Body:', req.body);
 
-    if (req.method === 'OPTIONS') {
-      // CORS Preflight
-      res.send();
-    } else {
-      console.log('Query:', req.query);
-      console.log('Body:', req.body);
+        var targetURL = process.env.MERCURY_BASE_URL;
+        var url = targetURL + req.url;
+        console.log('Request:', url);
 
-      var targetURL = process.env.MERCURY_BASE_URL;
-      var url = targetURL + req.url;
-      console.log('Request:', url);
+        const headers = {
+          authorization: req.header('authorization'),
+          'x-date': req.header('x-date'),
+          'content-type': req.get('content-type')
+            ? req.get('content-type')
+            : 'application/json',
+        };
 
-      const headers = {
-        authorization: req.header('authorization'),
-        'x-date': req.header('x-date'),
-        'content-type': req.get('content-type')
-          ? req.get('content-type')
-          : 'application/json',
-      };
+        const options = {
+          method: req.method,
+          url,
+          headers,
+          ody: JSON.stringify(req.body),
+        };
 
-      const options = {
-        method: req.method,
-        headers,
-        body:
-          req.get('content-type') === 'application/json'
-            ? JSON.stringify(req.body)
-            : req.body,
-      };
-
-      fetch(url, options)
-        .then((r) =>
-          r.headers.get('content-type') === 'application/json'
-            ? r.json()
-            : r.text()
-        )
-        .then((body) => res.status(200).send(body));
-    }
+        request(options, function (error, response, body) {
+          console.log(body);
+          if (error) {
+            console.error('error: ' + response.statusCode);
+          }
+        }).pipe(res);
+      }
+    });
   });
